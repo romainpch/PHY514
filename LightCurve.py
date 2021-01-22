cbad9ddb190e01034daa9896252b53557b3f1e83
romainpch <romain.pioch@ensta-paris.fr>
2021-01-22 10:38:34 +0100

    changement du nom des fonctions, simplifications et corrections, cas test simple sans skyfield

--- LightCurve.py
+++ LightCurve.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 from skyfield.api import load, Topos, EarthSatellite
 from astropy import units as u
 
+
+###########################################
+#  Frame changements and basic functions  #
+###########################################
+
+
 def normalize(vector) :
     return vector / np.linalg.norm(vector)
 
-
-####################################################
-#Fonctions à coder / à verifier :
 def I2BF(X, q) :
     #Cette fonction qui vient de visualisation.py passe de X dans le BF à a dans le ECI, il faudrait l'inverser 
     '''Body frame (cartesian) to Earth Centered Inertial frame (cartesian) thanks to the quaternion of the rotation'''
@@ -23,7 +26,6 @@ def I2BF(X, q) :
     a = np.dot(X_vect,P)
     return((a[0],a[1],a[2]))
 
-#Fonctions de conversion à vérifier, normalement c'est OK!
 def sph2cart(Xsph) :
     rho, theta, phi = Xsph
     x = rho*sin(theta)*cos(phi)
@@ -44,30 +46,58 @@ def radec2sph(Xradec) :
 def radec2cart(Xradec) :
     Xsph = radec2sph(Xradec)
     return sph2cart(Xsph)
-####################################################
+
+
+
+##############################################
+#  Definition of space objects and geometry  #
+##############################################
 
 
 class Surface :
-    def __init__(self,vertices, normal=None, albedo=0.2) :
+    def __init__(self,vertices, normal_vector=None, albedo=0.2) :
         self.P1 = vertices[0]
         self.P2 = vertices[1]
         self.P3 = vertices[2]
-        self.normal_vector = normalize(np.cross(self.P2-self.P1,self.P3-self.P2)) if normal is None else self.normal_vector = normalize(normal)
+        if normal_vector is None:
+            self.normal_vector = normalize(np.cross(self.P2-self.P1,self.P3-self.P2))
+        else : 
+            self.normal_vector = normal_vector
         self.area = 0.5*np.linalg.norm(np.cross(self.P2-self.P1,self.P3-self.P1))
         self.albedo = albedo
 
-
-class Object :
-    def __init__(self,surf_list) :
-        self.surf_list = surf_list
+class SpaceObject :
+    def __init__(self,geometry=None,nb_surf=0) :
+        self.geometry = geometry
+        self.nb_surf = nb_surf
+        self.position = np.array([0,0,0])
 
     def setObj(self,sat) :
         self.realobj = sat
-    
-    def setOrientation(self,orientation_quaternion) :
-        self.orientation_quaternion = orientation_quaternion
 
-    def computeBoxMesh(self, dim_x,dim_y,dim_z) :
+    def computeSphereMesh(self,radius, N) :
+        #a completer en prenant en compte N faces et non uniquement 264
+        l_surf = []
+        top = 0,0,radius
+        bottom = 0,0,-radius
+        angle = np.pi/12
+        
+        m_vertex=[[(round(radius*np.sin(j*angle)*np.cos(2*i*angle),16),\
+            round(radius*np.sin(j*angle)*np.sin(2*i*angle),16),\
+            round(radius*np.cos(j*angle),16))\
+            for i in range(13)] for j in range(1,12)]   #On cree tous les points de la triangulation
+
+        for i in range(12): #Ajout des faces top et bottom
+            l_surf.append(Surface(np.array([top,m_vertex[0][i+1],m_vertex[0][i]])))
+            l_surf.append(Surface(np.array([bottom,m_vertex[-1][i],m_vertex[-1][i+1]])))
+        
+        for j in range(0,10):   #Ajout de toutes les faces latérales
+            for i in range(12):
+                l_surf.append(Surface(np.array([m_vertex[j][i],m_vertex[j][i+1],m_vertex[j+1][i]])))
+                l_surf.append(Surface(np.array([m_vertex[j][i+1],m_vertex[j+1][i+1],m_vertex[j+1][i]])))
+        self.surf_list = l_surf
+
+    def computeBoxMesh(self,dim_x,dim_y,dim_z) :
         x,y,z = dim_x/2, dim_y/2, dim_z/2
         l_surf = []
         #Top and bottom
@@ -87,83 +117,59 @@ class Object :
         l_surf.append(Surface(np.array([[x,-y,z],[x,y,z],[x,-y,-z]]), np.array([1,0,0])))
         self.surf_list = l_surf  
 
-    def computeSphereMesh(self, radius, N) :
-        #a completer en prenant en compte N faces et non uniquement 264
-        #il faut aussi changer les lignes l_surf.append(Surface en mettant des array + le vecteur normal pour aller avec la syntaxe de Surface
-        l_surf = []
-        top = 0,0,radius
-        bottom = 0,0,-radius
-        angle = np.pi/12
-        
-        m_vertex=[[(round(radius*np.sin(j*angle)*np.cos(2*i*angle),16),\
-            round(radius*np.sin(j*angle)*np.sin(2*i*angle),16),\
-            round(radius*np.cos(j*angle),16))\
-            for i in range(13)] for j in range(1,12)]   #On cree tous les points de la triangulation
-
-        for i in range(12): #Ajout des faces top et bottom
-            l_surf.append(Surface(top,m_vertex[0][i+1],m_vertex[0][i]))
-            l_surf.append(Surface(bottom,m_vertex[-1][i],m_vertex[-1][i+1]))
-        
-        for j in range(0,10):   #Ajout de toutes les faces latérales
-            for i in range(12):
-                l_surf.append(Surface(m_vertex[j][i],m_vertex[j][i+1],m_vertex[j+1][i]))
-                l_surf.append(Surface(m_vertex[j][i+1],m_vertex[j+1][i+1],m_vertex[j+1][i]))
-        self.surf_list = l_surf
-
-
-class Sphere(Object) :
+class SphereSatellite(SpaceObject) :
     def __init__(self,radius, nb_surf=264) :
+        SpaceObject.__init__(self,'sphere',nb_surf)
+        SpaceObject.computeSphereMesh(self,radius,nb_surf)
         self.r = radius
         self.nb_surf = nb_surf
-        Object.computeSphereMesh(self,r,N)
-
-
-class Box(Object) :
+        
+class BoxSatellite(SpaceObject) :
     def __init__(self,dim_x,dim_y,dim_z) :
-        self.nb_surf = 8
-        Object.computeBoxMesh(self,dim_x,dim_y,dim_z)
+        SpaceObject.__init__(self,'box', 8)
+        SpaceObject.computeBoxMesh(self,dim_x,dim_y,dim_z)
+        self.dimensions = [dim_x,dim_y,dim_z]
+
+######################################
+#  Functions to compute LightCurves  #
+######################################
 
 
-def f(alpha, A_0=0.5, D=0.1, k=-0.5) :
+def phase_function(alpha, A_0=0.5, D=0.1, k=-0.5) :
     return A_0*exp(-alpha/D) + k*alpha + 1
 
-
-#Les luminosités de Lommel-Seeliger et Lambert S_LS et S_L sont à coder
-#Cependant elles fonctionnenet bien pour les planetes et astéoides
+#Les BRDF de Lommel-Seeliger et Lambert S_LS et S_L fonctionnent bien pour les planetes et astéoides
 #Pour des satellites, préférer les modèles de Ashikhmin-Shirley et Cook-Torrance (a voir comment implémenter ça dans un second temps)
-def uB(loc,t,q) :
-    # pos_radec = loc.radec() #Obtenir ici les coordonées de loc sous forme np.array([r, ra, dec])
-    pos_radec = loc.at(t).position.to(u.au).value
-    return normalize(I2BF(radec2cart(pos_radec),q))
+def brdf_function(mu,mu_0, alpha, c=0.1) :
+    return phase_function(alpha)*mu*mu_0*(c + 1/(mu + mu_0))
 
-def S_LS(mu, mu_0) :
-    # a coder, 
-    return 1.0 
-
-def S_L(mu, mu_0) :
-    return 1.0
-
-def S(mu,mu_0, alpha, c=0.1) :
-    # return f(alpha)*(S_LS(mu,mu_0) + c*S_L(mu,mu_0)) # marchera une fois que S_LS et S_L seront codées 
-    return f(alpha)*mu*mu_0*(c + 1/(mu + mu_0))
-
-def L(object, obs, sun, t) :
-    alpha = phase_angle(object.realobj, obs, t).to(u.deg).value
+def brightness(satellite, alpha,  observer_BF, sun_BF) :
     res = 0
-    for surf in object.surf_list :
+    for surf in satellite.surf_list :
         uB_n = surf.normal_vector
-        uB_obs = uB(obs,t,object.orientation_quaternion)
-        uB_sun = uB(sun,t,object.orientation_quaternion)
-
+        uB_obs = observer_BF #Because uB_obs is the vector from the observer to the target, and target is at [0,0,0] in the BF
+        uB_sun = sun_BF
+        
+        
         mu = np.dot(uB_n, uB_obs)
         mu_0 = np.dot(uB_n, uB_sun)
-        res += S(mu,mu_0, alpha)*surf.albedo*surf.area
+        if mu*mu_0 == 0 :
+            S = 0
+        else :
+            S = brdf_function(mu,mu_0, alpha)*surf.albedo*surf.area
+        res += S
     return res
 
 def magnitude(L, d, m_ref = -26.7) :#référence par rapport au soleil
     '''Magnitude of an object of luminosity L at a distance d with respect to a reference object of magnitude m_ref'''
     E = L/(4*pi*d**2)
     return m_ref - 2.5*log(E)/log(10.)
+
+
+####################
+#  Skyfield part   #
+####################
+
 
 ts=load.timescale()
 eph=load('de421.bsp')
@@ -179,23 +185,65 @@ def phase_angle(sat,loc,t,sol=sun):
     
 iss = EarthSatellite('1 25544U 98067A   21011.39930122  .00001177  00000-0  29227-4 0  9996','2 25544  51.6469  38.2316 0000504 209.0007 284.1827 15.49278328264297',name='iss')
 t=ts.utc(2021,1,11,16,range(30))
-# separations = phase_angle(iss,palaiseau,t)
 
 
-#Génération d'une courbe de lumière :
-satellite = Box(1.,1.,1.)
-satellite.setObj(iss)
-q_list = np.ones((len(t),4))
+###########################
+#  LightCurve generation  #
+###########################
 
-lightcurve = []
-for i in range(len(t)) :
-    q = q_list[i]
-    satellite.setOrientation(q)
-    geocentric_pos = satellite.realobj.at(t[i]).position.m
-    lightcurve += [magnitude(L(satellite,palaiseau,sun,t[i]),np.linalg.norm(geocentric_pos))]
 
-plt.plot(t.tt,lightcurve)
+# On se donne un référentiel inertiel simplifié 
+# Le satellite est un cube sat de 1m cube
+# L'observateur, le soleil et le satellite sont fixes dans ce référentiel
+# Ils ont pour positions Terre_IF(0,0,0)
+#                        Soleil_IF(0,√2,0)
+#                        Satellite_IF(√2/2,√2/2,0)
+# L'angle de phase vaut toujours π/2
+#
+# Le BF du satelite est le référentiel inertiel translaté de telle sorte à ce que la position du satellite dans le BF soit (0,0,0)
+# Ainsi, dans le BF, les positions sont à l'instant t : 
+#                        Terre_BF(cos(ωt+5π/4),sin(ωt+5π/4),0)
+#                        Soleil_BF(cos(ωt+3π/4),sin(ωt+3π/4),0)
+#                        Satellite_BF(0,0,0)
+#
+# Le satellite tourne selon l'axe z_BF (= z_IF) à une vitesse de 1 tour par seconde -> ω = 2π
+# La durée d'observation est 5sec, la fréquence d'échantillonage est 100Hz
+
+duration = 5
+f_sample = 100
+times = np.linspace(0,5,num=duration*f_sample )
+
+satellite = BoxSatellite(1.,1.,1.) #1m cubesat
+omega = 2*pi
+earth_phase = 5*pi/4
+sun_phase = 3*pi/4
+alpha = pi/2
+
+LightCurve = []
+for t in times :
+    uBF_sun = np.array([cos(omega*t + sun_phase), sin(omega*t + sun_phase), 0])
+    uBF_earth = np.array([cos(omega*t + earth_phase), sin(omega*t + earth_phase), 0])
+
+    S = brightness(satellite,alpha,uBF_earth,uBF_sun)
+    LightCurve += [S]
+
+
+plt.plot(times[1:-1],LightCurve[1:-1])
 plt.show()
+
+# Le Satellite tourne autour de son axe y 
+
+# q_list = np.ones((len(t),4))
+
+# lightcurve = []
+# for i in range(len(t)) :
+#     q = q_list[i]
+#     satellite.setOrientation(q)
+#     geocentric_pos = satellite.realobj.at(t[i]).position.m
+#     lightcurve += [magnitude(L(satellite,palaiseau,sun,t[i]),np.linalg.norm(geocentric_pos))]
+
+# plt.plot(t.tt,lightcurve)
+# plt.show()
 
 
 
