72f1a27e6e09ba55ab8f646fdc8c76218ebf8072
romainpch <romain.pioch@pm.me>
2021-03-09 18:23:43 +0100

    Ajout des algorithmes de Lafler-Kinman et Lomb  Scargle pour l'analyse des courbes de lumière

--- LightCurve.py
+++ LightCurve.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from skyfield.api import load, Topos, EarthSatellite
 from astropy import units as u
 from scipy.spatial.transform import Rotation as R
+from scipy.signal import lombscargle
+import random as rd
+
 #custom libraries
 from chg_frame import *
 from SpaceObjectsGeometry import *
@@ -80,12 +83,12 @@ def generate_attitude_list(rot_init, rot_vect, n_iter):
 ###########################################
 
 duration = 1
-f_sample = 200
+f_sample = 1000
 alpha_deg = 95 #required to compare with Bradley and Axelrad results
 satellite = BoxSatellite(3.,1.,1.)
 #satellite = SphereSatellite(1.)
 #satellite = CylinderSatellite(1.,1,1000)
-w = 2*pi #satellite rotation
+w = 10*pi #satellite rotation
 
 #Generating times and positions for the given phase angle
 times = np.linspace(0,duration,num=duration*f_sample )
@@ -94,8 +97,8 @@ sat_pos = np.array([0.,0.,0.])
 sun_pos =np.array([1.,0.,0.])
 obs_pos =np.array([cos(alpha_rad),sin(alpha_rad),0])
 
-alea = False #rot_axis is alea
-Nb_genere = 20 #nb of generated lightcurves
+alea = True #rot_axis is alea
+Nb_genere = 1 #nb of generated lightcurves
 
 #plt.title("Light curve for Sphere Saatellite at phase angle : " + str(alpha_deg) + "deg")
 
@@ -111,12 +114,83 @@ else :
     lightcurve = [luminosity(satellite,sat_pos,sun_pos,obs_pos,q) for q in q_list]
     plt.plot([360*t for t in times],lightcurve)
 
-plt.xlabel('Rotational phase (deg)')
-plt.ylabel('Light curve')
+# plt.xlabel('Rotational phase (deg)')
+# plt.ylabel('Light curve')
+# plt.show()
+
+
+
+#########################
+#  LightCurve analysis  #
+#########################
+
+def phase_folding(times,lightcurve,period,verbose=False): #Effectue le calcul des phases pour chaque point de mesure et les trie
+    phases = [(t % period)/period for t in times]
+    if verbose:
+        print(phases)
+    sorted_values = [val for _,val in sorted(zip(phases, lightcurve))]
+    return sorted(phases),sorted_values
+
+def phase_dispersion(sorted_values,mean=None): #Effectue le calcul de l'inverse du facteur de dispersion d'une courbe
+    if mean is None:
+        mean = np.mean(lightcurve)
+    dispersion = 0
+    var = 0
+    n = len(sorted_values)
+    sorted_values.append(sorted_values[0])
+    for i in range(n):
+        dispersion += (sorted_values[i]-sorted_values[i+1])**2
+        var += (sorted_values[i]-mean)**2   #On pourrait pondérer en fonction de l'écart des phases
+    return var/dispersion
+
+def phase_reconstruction_diagram(times,lightcurve,periods): #Renvoie la liste des valeurs des facteurs de dispersion inverse correspondant à la liste de périodes en entrée
+    mean = np.mean(lightcurve)
+    phase_disp = [phase_dispersion(phase_folding(times,lightcurve,period)[1],mean) for period in periods]
+    return phase_disp
+
+def find_period(periods,phase_disp):    #Détermine la période pour laquelle l'inverse du facteur de dispersion est maximal
+    t,m = max(zip(periods,phase_disp), key=(lambda x: x[1]))
+    return t
+
+def truncate(times,lightcurve,prop):    #Rééchantillone un jeu de données en choisissant aléatoirement une proportion donnée de ses valeurs
+    n=len(times)
+    sample = rd.sample(list(zip(times,lightcurve)),int(n*prop))
+    sample.sort(key=(lambda x: x[0]))
+    return [a for a,b in sample],[b for a,b in sample]
+
+
+#Plot des courbes de lumière
+plt.plot(times,lightcurve,color='r')
+times_test,lightcurve_test = truncate(times,lightcurve,0.1)
+lightcurve_test = [val+0.02*(rd.random()-1/2) for val in lightcurve_test]
+plt.xlabel('Temps (s)')
+plt.ylabel('Intensité lumineuse relative')
+plt.plot(times_test,lightcurve_test,color='b')
 plt.show()
 
+#Calcul de la meileure période
+log_periods = [k/100 for k in range(-200,0)]
+periods = [10**l for l in log_periods]
+freqs = [1/t for t in periods]
+#Méthode de Lafler-Kinman
+phase_disp = phase_reconstruction_diagram(times_test,lightcurve_test,periods)
+prm, = plt.plot(freqs,normalize(phase_disp),color='b')
+#Méthode de Lomb-Scargle
+phase_disp_ls = lombscargle(times_test,lightcurve_test,[2*pi/t for t in periods])
+ls, = plt.plot(freqs,normalize(phase_disp_ls),color='r')
+#Combinaison
+combine = [a*b for (a,b) in zip(phase_disp,phase_disp_ls)]
+tot, = plt.plot(freqs,normalize(combine),color='g')
+#Affichage
+plt.xscale('log')
+plt.xlabel('Fréquence (Hz)')
+plt.legend([prm,ls,tot],['Lafler-Kinman','Lomb-Scargle','Combinaison'])
+plt.show()
 
-
-######################################
-#  LightCurve generation : Skyfield  #
-######################################
+#Plot du diagramme de phase correspondant à la meilleure période
+t = find_period(periods,combine)
+plt.plot(*phase_folding(times_test,lightcurve_test,t),color='b')
+plt.plot(*phase_folding(times,lightcurve,t),color='r')
+plt.xlabel('Phase')
+plt.ylabel('Intesité lumineuse relative')
+plt.show()
